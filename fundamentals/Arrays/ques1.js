7f5ac38d95740ebf765487d923b309ed73f42293
vikramnirwan <vnirwan642@gmail.com>
2022-06-18 21:32:46 +0530

    commit

---
+++ fundamentals/Arrays/ques1.js
@@ -0,0 +1,3 @@
+let myColor = ["Red","Green","White","Black"];
+console.log(myColor.join());
+console.log(myColor.join("+"));
---
+++ fundamentals/operators and strings/ques1.js
@@ -0,0 +1,17 @@
+//slice practice
+//let name1 = "viKrAm" //Vikram
+//console.log(`${name1.charAt(0).toUpperCase()}${name1.slice(1).toLowerCase()
+//}`)
+
+//replace function 
+//let name1 = "raju dhobi"
+//let name2 = name1.replace("dhobi","payak")
+//console.log(name2)
+
+//string to array
+// converting the string to an array
+//const result4 = text1.split();
+
+let a = "one"
+let b = "8"
+console.log(a<b)
