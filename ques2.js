bba40b7990b997fe95ea50d914e3a80ad33ffddd
Vikram Nirwan <106387634+vikramnirwan@users.noreply.github.com>
2022-06-18 21:41:36 +0530

    Add files via upload

---
+++ ques2.js
@@ -0,0 +1,2 @@
+let strings = ["avengers","captain america","ironman","black panther"]
+console.log(strings.map(string=>string.toUpperCase()))
---
+++ ques3.js
@@ -0,0 +1,3 @@
+const inputWords = ["spray", "limit", "elite", "exuberant", "destruction", "present"]
+const outputWords = inputWords.filter(word=>word.length>6)
+console.log(outputWords)
